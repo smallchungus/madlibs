ec3c5cc03909cdf01e54b2f77243005987b0e222
smallchungus <wchen1396@gmail.com>
2022-10-08 09:14:55 -0400

    Project madlibs that's pretty much it learned string concatenation and making sure that I'm not a python noob god why didn't i start with python.

---
+++ main.py
@@ -0,0 +1,8 @@
+verb = input("Verb: \n")
+adjective = input("Adjective: \n")
+noun = input("Noun: \n")
+print(verb)
+print(adjective)
+print(noun)
+print("The " + adjective + " " + noun + " " + verb + " " + " across the moon like he was zooming I mean my god"
+                                                           "so this should be madlibs and that's pretty much it. \n")
