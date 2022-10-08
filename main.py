d84d0600e56bd5b9b7f3d007373e2706f52a33fc
smallchungus <wchen1396@gmail.com>
2022-10-08 09:23:46 -0400

    Okay I made it better using fstrings so now it's extra good. Okay even better good.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-verb = input("Verb: \n")
-adjective = input("Adjective: \n")
-noun = input("Noun: \n")
-print(verb)
-print(adjective)
-print(noun)
-print("The " + adjective + " " + noun + " " + verb + " " + " across the moon like he was zooming I mean my god"
-                                                           "so this should be madlibs and that's pretty much it. \n")
+verb = input("Verb: ")
+adj = input("Adjective: ")
+noun = input("Noun: ")
+
+madlib = f"The {adj} {noun} {verb} across the moon like he was zooming I mean my god so this should be madlibs and that's pretty much it. \n"
+print(madlib)
